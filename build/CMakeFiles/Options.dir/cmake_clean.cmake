ae34dae254b19bc106d6375ea9cc9552c3d08fa2
Sethvan <sethvan@yahoo.com>
2021-06-19 20:07:29 -0600

    Moved myGets() to OperationsDV1 files

---
+++ build/CMakeFiles/Options.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Options.dir/OperationsDV1.c.o"
+  "CMakeFiles/Options.dir/main.c.o"
+  "Options"
+  "Options.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Options.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
